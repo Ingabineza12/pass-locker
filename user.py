0b93db597e2d66daedf49b693cdc88272977b490
Deborah M Ingabire <ingabineza@gmail.com>
2020-11-15 08:57:08 +0200

    added method for deleting user

--- user.py
+++ user.py
@@ -6,14 +6,23 @@ class User:
     """
     user_list=[] #empty user list
 
-    def __init__(self,first_name,username,password,email):
-
-        self.first_name=first_name
-        self.username=username
-        self.password=password
-
     def save_user(self):
         '''
         save user method saves user objects into user list
         '''
         User.user_list.append(self)
+
+
+    def delete_user(self):
+        '''
+        delete_user method delete a saved used from user
+        '''
+        User.user_list.remove(self)
+
+        
+
+    def __init__(self,first_name,username,password,email):
+
+        self.first_name=first_name
+        self.username=username
+        self.password=password
