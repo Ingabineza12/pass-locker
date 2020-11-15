a71c13a67c93e576553007a85b48ce9c0eff3fc9
Deborah M Ingabire <ingabineza@gmail.com>
2020-11-15 10:48:17 +0200

    find if user exit and how to find user by name

--- user.py
+++ user.py
@@ -26,6 +26,34 @@ class User:
         '''
         return cls.user_list
 
+     @classmethod
+     def user_exist(cls,first_name):
+        '''
+        method that find by username and return all information that matches
+        Args:
+            name: first name to search for
+        Returns:
+            all information of that person
+        '''
+        for user in cls.user_list:
+            if user.first_name== first_name:
+                return True
+        return False
+
+    @classmethod
+    def find_by_name(cls,first_name):
+        '''
+        method that find by username and return all information that matches
+        Args:
+            name: first name to search for
+        Returns:
+            all information of that person
+        '''
+        for user in cls.user_list:
+            if user.first_name==first_name:
+                return user
+
+
     def __init__(self,first_name,username,password,email):
 
         self.first_name=first_name
