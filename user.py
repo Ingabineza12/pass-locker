861109e6a31d1acc5ab4989313395afd808549cd
Deborah M Ingabire <ingabineza@gmail.com>
2020-11-15 08:51:28 +0200

    added init

--- user.py
+++ user.py
@@ -5,3 +5,9 @@ class User:
     class that generates a new instance of a user
     """
     user_list=[] #empty user list
+
+    def __init__(self,first_name,username,password,email):
+
+        self.first_name=first_name
+        self.username=username
+        self.password=password
